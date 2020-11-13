e9940bafd3ff2493bf6c9bda2204b075d1e021dd
Carlo Malvicini <S4708696@studenti.unige.it>
2020-11-13 18:43:49 +0100

    aggiunta prova per colleghi

--- TAP1718_Laboratorio3_Git/Program.cs
+++ TAP1718_Laboratorio3_Git/Program.cs
@@ -8,6 +8,7 @@ namespace TAP1718_Laboratorio3_Git
         {
             Console.WriteLine("Se leggete questo messaggio, significa che siete tornati indietro nel tempo!");
             Console.WriteLine("end lab1");
+            Console.WriteLine("Prova progetto saw");
         }
     }
 }
